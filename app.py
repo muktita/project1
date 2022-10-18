490b243e7a54bcceb1c6abaaa6e82c23eec95287
ZyklonR <92772530+ZyklonR@users.noreply.github.com>
2022-10-18 16:37:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -36,43 +36,6 @@ def ok():
     return "ehllo"
 
 
-words = ['programming', 'tiger', 'lamp', 'television',
-'laptop', 'water', 'microscope', 'doctor', 'youtube',
-'projects']
-
-random_word = random.choice(words)
-
-print('our random word', random_word)
-
-print('*********** WORD GUESSING GAME ***********')
-
-user_guesses = ''
-chances = 10
-
-while chances > 0:
-    wrong_guesses = 0
-    for character in random_word:
-        if character in user_guesses:
-            print(f"Correct guess: {character}")
-        else:
-            wrong_guesses += 1
-            print('_')
-
-    if wrong_guesses == 0:
-        print("Correct.")
-        print(f"Word : {random_word}")
-        break
-    guess = input('Make a guess: ')
-    user_guesses += guess
-
-    if guess not in random_word:
-        chances -= 1
-        print(f"Wrong. You have {chances} more chances")
-
-        if chances == 0:
-            print('game over')
-
-
 # Start the server
 if __name__ == "__main__":
    app.run(debug = True, port=5000)
