f51915fce926afc1c6b1def508c4cbd7618f3d86
Muktita Kim <muktitakim@csu.fullerton.edu>
2022-10-19 15:29:34 -0700

    populate the tables

--- populateQuery.py
+++ populateQuery.py
@@ -1,4 +1,5 @@
 import json
+from opcode import opname
 import sqlite3
 
 conn = sqlite3.connect('wordle.db') # Create a database to store the db
@@ -6,11 +7,17 @@ c = conn.cursor()
 
 # Open and load the json file
 f = open('correct.json')
-data = json.load(f)
+data = json.loads(f)
 
 # Populate the correct_words
 for val in data:
-    c.execute("INSERT INTO game (correct_word) VALUES ()", val)
+    c.execute("INSERT INTO game (correct_word) VALUES (?)", val)
+f.close()
 
+g = open('valid.json')
+validData = json.load(g)
+
+for val in validData:
+    c.execute("INSERT INTO game (valid_guess) VALUES (?)", val)
+g.close()
 conn.commit
-c.close()
