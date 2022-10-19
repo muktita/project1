39a40d2ceac089347a2c8100df730f402c0662d8
Muktita Kim <muktitakim@csu.fullerton.edu>
2022-10-19 14:34:59 -0700

    Database Table

--- wordle.sql
+++ wordle.sql
@@ -1,20 +1,35 @@
 /*Database For Wordle */
+-- To enable foreign_keys
+PRAGMA foreign_keys = ON;
 
 /* Table for User*/
 DROP TABLE IF EXISTS user;
 CREATE TABLE user (
-    id int primary key,
+    userid integer primary key,
     username varchar,
     pwd varchar,
+    UNIQUE(username)
 );
 
 /* Table for Game*/
 DROP TABLE IF EXISTS game;
 CREATE TABLE game (
-    id itn primary key,
-    user_id int references user(id),
+    gameid integer primary key,
+    userid integer,
     correct_word varchar,
     win boolean,
-    guess int
+    num_guess integer,
+    Foreign KEY (userid) REFERENCES (userid)
 );
+
+/* Table for Game session*/
+DROP TABLE IF EXISTS gameSession;
+CREATE TABLE gameSession (
+    userid integer,
+    gameid integer,
+    user_guess varchar,
+    FOREIGN KEY (userid) REFERENCES (userid),
+    FOREIGN KEY (gameid) REFERENCES (gameid)
+);
+
 
